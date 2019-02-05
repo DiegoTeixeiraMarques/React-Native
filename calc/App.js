f4fee52ebaede63ef2d7d9676e5cb57189739e95
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-05 00:58:10 -0200

    Pulling aero function to use label of the button component

--- calc/App.js
+++ calc/App.js
@@ -8,27 +8,40 @@ export default class App extends Component {
     displayValue: '0'
   }
 
+  addDigit = n => {                                                             // Add digito quando clicado
+    this.setState({ displayValue: n })
+  }
+
+  clearMemory = () => {                                                         // Limpa tela da calculadora quando solicitado
+    this.setState ({ displayValue: '0' })
+  }
+
+  setOperation = operation => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Display value={this.state.displayValue}/>        {/* Recebe o estado inicial do App, '0' */}
+        <Display value={this.state.displayValue}/>                              {/* Recebe o estado inicial do App, '0' */}
         <View style={styles.buttons}>
-        <Button label='AC'/>
-        <Button label='/'/>
-        <Button label='7'/>
-        <Button label='8'/>
-        <Button label='9'/>
-        <Button label='*'/>
-        <Button label='4'/>
-        <Button label='5'/>
-        <Button label='6'/>
-        <Button label='-'/>
-        <Button label='1'/>
-        <Button label='2'/>
-        <Button label='3'/>
-        <Button label='+'/>
-        <Button label='0'/>
-        <Button label='.'/>
+        <Button label='AC' triple onClick={this.clearMemory} />
+        <Button label='/' operation onClick={this.setOperation} />              {/* lable é capturado pelo componente Button para mostrar em tela quando clicado*/}
+        <Button label='7' onClick={this.addDigit} />                            {/* lable é capturado pelo componente Button para mostrar em tela quando clicado*/}
+        <Button label='8' onClick={this.addDigit} />                            {/* lable é capturado pelo componente Button para mostrar em tela quando clicado*/}
+        <Button label='9' onClick={this.addDigit} />
+        <Button label='*' operation onClick={this.setOperation} />              
+        <Button label='4' onClick={this.addDigit} />
+        <Button label='5' onClick={this.addDigit} />
+        <Button label='6' onClick={this.addDigi} />
+        <Button label='-' operation onClick={this.setOperation} />
+        <Button label='1' onClick={this.addDigit} />
+        <Button label='2' onClick={this.addDigit} />
+        <Button label='3' onClick={this.addDigit} />
+        <Button label='+' operation onClick={this.setOperation} />
+        <Button label='0' double onClick={this.addDigit} />
+        <Button label='.' onClick={this.addDigit} />
+        <Button lable='=' operation onClick={this.setOperation} />
         </View>
       </View>
     );
