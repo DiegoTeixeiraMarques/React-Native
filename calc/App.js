73d690d0be6d6815a00d7653a0f94eea360a7aff
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-06 01:02:25 -0200

    Method addDigit, new value

--- calc/App.js
+++ calc/App.js
@@ -25,6 +25,12 @@ export default class App extends Component {
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+    }
   }
 
   clearMemory = () => {                                                         // Limpa tela da calculadora quando solicitado
