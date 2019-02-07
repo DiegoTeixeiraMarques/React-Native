31e24468da819a04ecf3feca12a8b272485e893f
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-02-07 19:59:03 -0200

    method setOperation, finalized

--- calc/App.js
+++ calc/App.js
@@ -40,6 +40,29 @@ export default class App extends Component {
 
   setOperation = operation => {
 
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+
+      } catch (e) {
+        values[0] = this.state.values[0]
+      
+      }
+
+      values[1] = 0
+      this.setState({ 
+        displayValue: calues[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,                                                     // Limpa o display após apertar "=" e inserir outro valor
+        //clearDisplay: !equals,
+        values,
+       })
+    } 
   }
 
   render() {
